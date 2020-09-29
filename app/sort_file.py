b70023c624d2bc97f8a65073bf0b730fb95d119d
saoud <saoud.dev@gmail.com>
2020-09-29 19:54:14 +0000

    modification to check number of command line arguments in the sort_file.py app

--- app/sort_file.py
+++ app/sort_file.py
@@ -2,10 +2,13 @@
 import pandas as pd
 import sys
 
+if len(sys.argv) < 4:
+    print("Usage: sort_file.py inputfname outputfname sortby")
+    sys.exit()
+
 infname=sys.argv[1]
 outfname=sys.argv[2]
 sortby=sys.argv[3]
-
 df = pd.read_csv(infname)
 df.sort_values(inplace=True, by=sortby)
 df.to_csv(outfname, index=False)
